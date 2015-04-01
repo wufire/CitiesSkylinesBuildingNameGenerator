2963e48ef9929076e0b8873e88edbc079d0cb891
awu <wufire@gmail.com>
2015-04-01 01:29:41 -0500

    Whitespace changes. Removed some unused 'using'

--- WufireNameGenerator/WufireNameGenerator.cs
+++ WufireNameGenerator/WufireNameGenerator.cs
@@ -1,43 +1,1 @@
-using ICities;
-using System;
-using ColossalFramework.IO;
-namespace WufireNameGenerator
-{
-	public class WufireNameGenerator : IUserMod
-	{
-		public static string CommercialNameFile {
-			get {
-				return DataLocation.modsPath + "/WufireNameGenerator/CommercialNames.json";
-			}
-		}
-
-		public static string OfficeNameFile {
-			get {
-				return DataLocation.modsPath + "/WufireNameGenerator/OfficeNames.json";
-			}
-		}
-
-		public static string IndustryNameFile {
-			get {
-				return DataLocation.modsPath + "/WufireNameGenerator/IndustryNames.json";
-			}
-		}
-
-		public string Description
-		{
-			get
-			{
-				return "Creates new names for industrial, office and commercial buildings. Add your own business names in the mod folder!";
-			}
-		}
-		
-		public string Name
-		{
-			get
-			{
-				return "Wufire's Business Name Generator";
-			}
-		}
-	}
-}
-
+using ICities;using System;using ColossalFramework.IO;namespace WufireNameGenerator{	public class WufireNameGenerator : IUserMod	{		public static string CommercialNameFile {			get {				return DataLocation.modsPath + "/WufireNameGenerator/CommercialNames.json";			}		}		public static string OfficeNameFile {			get {				return DataLocation.modsPath + "/WufireNameGenerator/OfficeNames.json";			}		}		public static string IndustryNameFile {			get {				return DataLocation.modsPath + "/WufireNameGenerator/IndustryNames.json";			}		}		public string Description		{			get			{				return "Creates new names for industrial, office and commercial buildings. Add your own business names in the mod folder!";			}		}				public string Name		{			get			{				return "Wufire's Business Name Generator";			}		}	}}
